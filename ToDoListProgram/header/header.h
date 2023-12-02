a950358ef0b6287f8446f2b9884858844e81ba05
Nicholas Davies <141979976+nicholas-davies@users.noreply.github.com>
2023-12-01 21:12:41 -0800

    Add files via upload

--- ToDoListProgram/header/header.h
+++ ToDoListProgram/header/header.h
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
+class toDoList {
+
+    public:
+    
+        void addToDoItems(vector<string>& toDoItems);
+        void deleteToDoItems(vector<string>& toDoItems);
+
+};
